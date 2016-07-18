230b728aee1fc45fd2635e728f12cdc47c0e4206
nn4343 <gea01441@nifty.com>
2016-07-18 11:51:17 +0900

    Initial

---
+++ loadtest2/loadtest/test.js
@@ -0,0 +1,170 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var Util;
+(function (Util) {
+    var StateControl = (function () {
+        function StateControl() {
+        }
+        StateControl.prototype.Goto = function (state) {
+            this.m_nextstate = state;
+        };
+        StateControl.prototype.Update = function () {
+            if (this.m_nextstate != null) {
+                this.m_curstate = this.m_nextstate;
+                this.m_nextstate = null;
+                this.m_curstate(true);
+            }
+            else {
+                if (this.m_curstate != null) {
+                    this.m_curstate(false);
+                }
+            }
+        };
+        return StateControl;
+    }());
+    Util.StateControl = StateControl;
+    var StateItem = (function () {
+        function StateItem() {
+        }
+        return StateItem;
+    }());
+    Util.StateItem = StateItem;
+    var StateSequencer = (function () {
+        function StateSequencer() {
+            this.m_list = new Array();
+            this.m_elapsed = 0;
+            this.m_cur = null;
+            this.m_next = null;
+        }
+        StateSequencer.prototype.Command = function (state, p1, p2, p3, p4) {
+            if (p1 === void 0) { p1 = null; }
+            if (p2 === void 0) { p2 = null; }
+            if (p3 === void 0) { p3 = null; }
+            if (p4 === void 0) { p4 = null; }
+            var i = new StateItem();
+            i.self = this;
+            i.state = state;
+            i.p1 = p1;
+            i.p2 = p2;
+            i.p3 = p3;
+            i.p4 = p4;
+            this.m_list.push(i);
+        };
+        StateSequencer.prototype.Update = function (dt) {
+            this.m_elapsed += dt / 1000;
+            if (this.m_cur == null) {
+                if (this.m_list.length > 0) {
+                    this.m_next = this.m_list.shift();
+                }
+            }
+            if (this.m_next != null) {
+                this.m_cur = this.m_next;
+                this.m_next = null;
+                this.m_elapsed = 0;
+            }
+            if (this.m_cur != null) {
+                var i = this.m_cur;
+                if (i.state != null) {
+                    i.state(this.m_elapsed, i);
+                }
+            }
+        };
+        StateSequencer.prototype.Done = function () {
+            this.m_cur = null;
+        };
+        return StateSequencer;
+    }());
+    Util.StateSequencer = StateSequencer;
+})(Util || (Util = {}));
+var Sandbox;
+(function (Sandbox) {
+    var MainClass = (function (_super) {
+        __extends(MainClass, _super);
+        function MainClass() {
+            _super.apply(this, arguments);
+        }
+        MainClass.prototype.Start = function (canvas) {
+            this.m_canvas = canvas;
+            this.m_engine = new BABYLON.Engine(canvas, true);
+            this.m_scene = new BABYLON.Scene(this.m_engine);
+            this.m_scene.clearColor = new BABYLON.Color3(56 / 256, 87 / 256, 145 / 256);
+            this.m_camera = new BABYLON.ArcRotateCamera("camera1", 0, 0.8, 100, BABYLON.Vector3.Zero(), this.m_scene);
+            this.m_camera.attachControl(canvas);
+            var mesh = BABYLON.Mesh.CreateBox("box01", 50, this.m_scene);
+            var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), this.m_scene);
+            this.m_engine.enableOfflineSupport = false;
+            this.IssueCommands(this);
+        };
+        MainClass.prototype.runRenderLoop = function () {
+            var _this = this;
+            this.m_engine.runRenderLoop(function () {
+                _this.Update(_this.m_engine.getDeltaTime());
+                _this.m_scene.render();
+            });
+        };
+        //コマンド発行
+        MainClass.prototype.IssueCommands = function (p) {
+            var self = p;
+            self.Command(self.S_IDLE, 5);
+            self.Command(self.S_LOAD);
+            self.Command(self.S_IDLE, 5);
+            self.Command(self.S_CREATE_BASIC_SCENE);
+            self.Command(self.S_DONE);
+        };
+        // State
+        MainClass.prototype.S_IDLE = function (t, i) {
+            var p1 = i.p1;
+            if (t == 0) {
+                console.log("S_IDLE Initiallize");
+            }
+            else {
+                console.log("S_IDLE Running : " + t);
+                if (t > p1) {
+                    i.self.Done();
+                }
+            }
+        };
+        MainClass.prototype.S_LOAD = function (t, i) {
+            var self = i.self;
+            if (t == 0) {
+                this.bOk = false;
+                BABYLON.SceneLoader.Load("./", "egw.babylon", self.m_engine, function (scene) {
+                    self.m_scene = scene;
+                    self.bOk = true;
+                });
+            }
+            else {
+                if (self.bOk) {
+                    self.Done();
+                }
+            }
+        };
+        MainClass.prototype.S_CREATE_BASIC_SCENE = function (t, i) {
+            var self = i.self;
+            if (t == 0) {
+                self.m_scene = new BABYLON.Scene(self.m_engine);
+                self.m_scene.clearColor = new BABYLON.Color3(56 / 256, 87 / 256, 145 / 256);
+                self.m_camera = new BABYLON.ArcRotateCamera("camera1", 0, 0.8, 100, BABYLON.Vector3.Zero(), self.m_scene);
+                self.m_camera.attachControl(self.m_canvas);
+                var mesh = BABYLON.Mesh.CreateBox("box01", 50, self.m_scene);
+                var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), self.m_scene);
+            }
+            else {
+                self.Done();
+            }
+        };
+        MainClass.prototype.S_DONE = function (t, i) {
+            var self = i.self;
+            if (t == 0) {
+                self.IssueCommands(self);
+                self.Done();
+            }
+        };
+        return MainClass;
+    }(Util.StateSequencer));
+    Sandbox.MainClass = MainClass;
+})(Sandbox || (Sandbox = {}));
+//# sourceMappingURL=test.js.map
---
+++ loadtest2/loadtest/test.ts
@@ -0,0 +1,186 @@
+﻿module Util {
+    export class StateControl {
+        m_curstate: any;
+        m_nextstate: any;
+
+        public Goto(state: any) {
+            this.m_nextstate = state;
+        }
+        public Update() {
+            if (this.m_nextstate != null) {
+                this.m_curstate = this.m_nextstate;
+                this.m_nextstate = null;
+                this.m_curstate(true);
+            }
+            else {
+                if (this.m_curstate != null) {
+                    this.m_curstate(false);
+                }
+            }
+        }
+    }
+
+    export class StateItem {
+        self: any;  // ※this対策
+        state: any;
+        p1: any;
+        p2: any;
+        p3: any;
+        p4: any;
+    }
+    export class StateSequencer {
+
+        private m_list: Array<StateItem>;
+        private m_elapsed: number;
+        private m_cur:  StateItem;
+        private m_next: StateItem;
+
+        constructor() {
+            this.m_list = new Array<StateItem>();
+            this.m_elapsed = 0;
+            this.m_cur  = null;
+            this.m_next = null;
+        }
+
+        public Command(
+            state: any,
+            p1: any = null,
+            p2: any = null,
+            p3: any = null,
+            p4: any = null
+        ):void {
+            var i = new StateItem();
+            i.self = this;
+            i.state = state;
+            i.p1 = p1;
+            i.p2 = p2;
+            i.p3 = p3;
+            i.p4 = p4;
+            this.m_list.push(i);
+        }
+
+        public Update(dt: number) {
+            this.m_elapsed += dt / 1000;
+
+            if (this.m_cur == null) {
+                if (this.m_list.length > 0) {
+                    this.m_next = this.m_list.shift();
+                }
+            }
+            if (this.m_next != null) {
+                this.m_cur = this.m_next;
+                this.m_next = null;
+                this.m_elapsed = 0;
+            }
+            if (this.m_cur != null) {
+                var i = this.m_cur;
+                if (i.state != null) {
+                    i.state(this.m_elapsed, i);
+                }
+            }
+        }
+
+        public Done(): void {
+            this.m_cur = null;
+        }
+    }
+}
+
+module Sandbox {
+
+    export class MainClass extends Util.StateSequencer {
+
+        public m_canvas: HTMLCanvasElement;
+        public m_engine: BABYLON.Engine;
+        public m_scene: BABYLON.Scene;
+        public m_camera: BABYLON.TargetCamera;
+
+        public Start(canvas: HTMLCanvasElement): void {
+            this.m_canvas = canvas;
+            this.m_engine = new BABYLON.Engine(canvas, true);
+            this.m_scene = new BABYLON.Scene(this.m_engine);
+            this.m_scene.clearColor = new BABYLON.Color3(56 / 256, 87 / 256, 145 / 256);
+            this.m_camera = new BABYLON.ArcRotateCamera("camera1", 0, 0.8, 100, BABYLON.Vector3.Zero(), this.m_scene);
+            this.m_camera.attachControl(canvas);
+            var mesh = BABYLON.Mesh.CreateBox("box01", 50, this.m_scene);
+            var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), this.m_scene);
+
+            this.m_engine.enableOfflineSupport = false;
+
+            this.IssueCommands(this);
+        }
+        public runRenderLoop(): void {
+            this.m_engine.runRenderLoop(() => {
+                this.Update(this.m_engine.getDeltaTime());
+                this.m_scene.render();
+            });
+        }
+
+        //コマンド発行
+        private IssueCommands(p: any): void {
+            var self: MainClass = p;
+            self.Command(self.S_IDLE, 5);
+            self.Command(self.S_LOAD);
+            self.Command(self.S_IDLE, 5);
+            self.Command(self.S_CREATE_BASIC_SCENE);
+            self.Command(self.S_DONE);
+        }
+
+        // State
+        private S_IDLE(t: number, i: Util.StateItem): void {
+            var p1: number = i.p1;
+            if (t == 0) {
+                console.log("S_IDLE Initiallize");
+            }
+            else {
+                console.log("S_IDLE Running : " + t);
+                if (t > p1) {
+                    i.self.Done();
+                }
+            }
+        }
+
+        private bOk: boolean;
+        private S_LOAD(t: number, i: Util.StateItem): void {
+            var self: MainClass = i.self;
+            if (t == 0) {
+                this.bOk = false;
+                BABYLON.SceneLoader.Load("./", "egw.babylon", self.m_engine,
+                    (scene: BABYLON.Scene) => {
+                        self.m_scene = scene;
+                        self.bOk = true;
+                    }
+                );
+            }
+            else {
+                if (self.bOk) {
+                    self.Done();
+                }
+            }
+        }
+
+        private S_CREATE_BASIC_SCENE(t: number, i: Util.StateItem): void {
+            var self: MainClass = i.self;
+            if (t == 0) {
+                self.m_scene = new BABYLON.Scene(self.m_engine);
+                self.m_scene.clearColor = new BABYLON.Color3(56 / 256, 87 / 256, 145 / 256);
+                self.m_camera = new BABYLON.ArcRotateCamera("camera1", 0, 0.8, 100, BABYLON.Vector3.Zero(), self.m_scene);
+                self.m_camera.attachControl(self.m_canvas);
+                var mesh = BABYLON.Mesh.CreateBox("box01", 50, self.m_scene);
+                var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), self.m_scene);
+            }
+            else {
+                self.Done();
+            }
+        }
+
+        private S_DONE(t: number, i: Util.StateItem): void {
+            var self: MainClass = i.self;
+            if (t == 0) {
+                self.IssueCommands(self);
+                self.Done();
+            }
+        }
+
+    }
+}
