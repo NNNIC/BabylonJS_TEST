ebd536a27c833e2f548bfc2eac7b91e5900b1ae8
nn4343 <gea01441@nifty.com>
2016-07-18 22:01:10 +0900

    スプライトテスト追加。

--- loadtest2/loadtest/test.js
+++ loadtest2/loadtest/test.js
@@ -109,6 +109,8 @@ var Sandbox;
         MainClass.prototype.IssueCommands = function (p) {
             var self = p;
             self.Command(self.S_IDLE, 2);
+            self.Command(self.S_SPlITE_TEST);
+            self.Command(self.S_IDLE, 5);
             self.Command(self.S_CREATE_PLANE_AND_LOAD_TEXTURE);
             self.Command(self.S_IDLE, 5);
             self.Command(self.S_CREATE_BASIC_SCENE2);
@@ -205,6 +207,62 @@ var Sandbox;
                 plane.rotation.x = Math.PI / 2;
                 self.m_mesh = plane;
                 var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), self.m_scene);
+            }
+            else {
+                self.Done();
+            }
+        };
+        //スプライトテスト
+        MainClass.prototype.S_SPlITE_TEST = function (t, i) {
+            var self = i.self;
+            if (t == 0) {
+                self.m_scene = new BABYLON.Scene(self.m_engine);
+                self.m_scene.clearColor = new BABYLON.Color3(56 / 256, 87 / 256, 145 / 256);
+                // Create camera and light
+                var light = new BABYLON.PointLight("Point", new BABYLON.Vector3(5, 10, 5), self.m_scene);
+                var camera = new BABYLON.ArcRotateCamera("Camera", 1, 0.8, 8, new BABYLON.Vector3(0, 0, 0), self.m_scene);
+                camera.attachControl(self.m_canvas, true);
+                // Create a sprite manager to optimize GPU ressources
+                // Parameters : name, imgUrl, capacity, cellSize, scene
+                var spriteManagerTrees = new BABYLON.SpriteManager("treesManager", "assets/palm.png", 2000, 800, self.m_scene);
+                //We create 2000 trees at random positions
+                for (var n = 0; n < 2000; n++) {
+                    var tree = new BABYLON.Sprite("tree", spriteManagerTrees);
+                    tree.position.x = Math.random() * 100 - 50;
+                    tree.position.z = Math.random() * 100 - 50;
+                    tree.isPickable = true;
+                    //Some "dead" trees
+                    if (Math.round(Math.random() * 5) === 0) {
+                        tree.angle = Math.PI * 90 / 180;
+                        tree.position.y = -0.3;
+                    }
+                }
+                //Create a manager for the player's sprite animation
+                var spriteManagerPlayer = new BABYLON.SpriteManager("playerManager", "assets/player.png", 2, 64, self.m_scene);
+                // First animated player
+                var player = new BABYLON.Sprite("player", spriteManagerPlayer);
+                player.playAnimation(0, 40, true, 100);
+                player.position.y = -0.3;
+                player.size = 0.3;
+                player.isPickable = true;
+                // Second standing player
+                var player2 = new BABYLON.Sprite("player2", spriteManagerPlayer);
+                player2.stopAnimation(); // Not animated
+                player2.cellIndex = 2; // Going to frame number 2
+                player2.position.y = -0.3;
+                player2.position.x = 1;
+                player2.size = 0.3;
+                player2.invertU = -1; //Change orientation
+                player2.isPickable = true;
+                // Picking
+                spriteManagerTrees.isPickable = true;
+                spriteManagerPlayer.isPickable = true;
+                self.m_scene.onPointerDown = function (evt) {
+                    var pickResult = self.m_scene.pickSprite(this.pointerX, this.pointerY);
+                    if (pickResult.hit) {
+                        pickResult.pickedSprite.angle += 0.5;
+                    }
+                };
             }
             else {
                 self.Done();
